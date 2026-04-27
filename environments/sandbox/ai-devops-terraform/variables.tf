83cc18e3571fc6b71f00d9ad3d288c125f26ec22
Ansu <ansuj9@yahoo.com>
2026-04-27 15:46:46 +0100

    chores: setting up function app

--- environments/sandbox/ai-devops-terraform/variables.tf
+++ environments/sandbox/ai-devops-terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "location" {
-  default = "canadacentral"
+  default = "eastus2"
 }
 
 variable "resource_group_name" {
@@ -7,5 +7,5 @@ variable "resource_group_name" {
 }
 
 variable "project_name" {
-  default = "aidevops01"
+  default = "aidevops-v2"
 }
